ac763a2aa9de6f2a5f34745ee45bc9f9f9bce51e
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2023-05-29 03:52:02 -0400

    Update index.js
    Signed-off-by: BestLeaks <59940456+BestLeaks@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+// Greet Message
+console.log('Thanks for using questions-and-facts Fact Bundle!')
+
+// Importing files
+// Fact Files
+const animalFacts = require('./directory/animal-facts.json')
+const carFacts = require('./directory/car-facts.json')
+const foodFacts = require('./directory/food-facts.json')
+
+// Code starts
+
+function fact () {
+  
+  let allFacts = [
+    animalFacts[Math.floor(Math.random() * emojiFacts.length)], carFacts[Math.floor(Math.random() * spaceFacts.length)], foodFacts[Math.floor(Math.random() * foodFacts.length)]
+  ]
+
+  return{
+    all: allFacts[Math.floor(Math.random() * allFacts.length)],
+    emoji: animalFacts[Math.floor(Math.random() * emojiFacts.length)],
+    space: carFacts[Math.floor(Math.random() * spaceFacts.length)],
+    food: foodFacts[Math.floor(Math.random() * foodFacts.length)]
+  }
+}
+
+// 5. Export all Modules
+module.exports = {
+  version: require('./package.json').version,
+  fact: fact
+}
 
